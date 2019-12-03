0e2bc8f33dc735ce749e951a8efa9c2085090846
RaphaelBento157 <rafhaesilvalbento@gmail.com>
2019-12-03 12:48:48 -0300

    Initial commit

---
+++ src/br/com/fis/view/Menu.java
@@ -0,0 +1,26 @@
+
+package br.com.fis.view;
+import java.util.Scanner;
+
+public class Menu {
+    public static void main(String[] args){
+        int caso;
+        System.out.print(" Bem Vindo \n Escolha uma opção a seguir:\n 1 - Deprtamento \n 2- Funcionário \n 3 - Gerentes \n 99 - sair");
+        Scanner sc=new Scanner(System.in);
+        caso=sc.nextInt();
+        switch(caso){
+        
+            case 1:
+                break;
+                
+            case 2:    
+                break;
+                
+            case 3:
+                break;
+            
+            case 4:
+                break;
+        }
+    }
+}
